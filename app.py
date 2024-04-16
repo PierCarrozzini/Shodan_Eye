4e0cff5a443f621991695edf5e1c54c8630048f7
theorly <79010526+theorly@users.noreply.github.com>
2024-04-16 11:54:33 +0200

    update

--- app.py
+++ app.py
@@ -1,35 +1,63 @@
-import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, render_template, request
+import shodan
 
 app = Flask(__name__)
+SHODAN_API_KEY = 'hJ4hcLWj7YK3PiIYKqhIaNf0Mw6uGNpQ'  # Replace 'your_api_key_here' with your actual Shodan API key
+api = shodan.Shodan(SHODAN_API_KEY)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/',  methods=['GET', 'POST'])
+def get_device_info(ip_address):
+    try:
+        host = api.host(ip_address)
+        print(host)  # Stampa le informazioni sull'host per il debug
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+        vulnerabilities = []
+        for vulnerability in host['vulns']:
+            print(vulnerability)  # Stampa ogni vulnerabilità per il debug
+            
 
+        return {
+            'ip': host['ip_str'],
+            'port': host['ports'],
+            'data': host['data'],
+            'vulnerabilities': host['vulns']
+        }
+    except shodan.APIError as e:
+        return {'error': str(e)}
+
+
+
+# Pagina per visualizzare le informazioni su un dispositivo specifico
+@app.route('/results', methods=['GET', 'POST'])
+def device_info():
+    if request.method == 'POST':
+        ip_address = request.form['ip_address']
+
+        # Otteniamo informazioni dettagliate sul dispositivo
+        device_info = get_device_info(ip_address)
+
+        return render_template('results.html', device_info=device_info)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
 
 
-# hi pier
+"""
+
+ALLORA SEARCH -> PER SCANSIONE DI TUTTI I DISPOSITIVI 
+
+IN PIU' SI IMPLEMENTA LA RICERCA SUL PROPRIO DEVICE 
+
+E POI IL SISTEMA DI ALERT IN DIRETTA!! 
+
+"""
